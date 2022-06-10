b419ed56b62542eaa1fce2c8c0d97f1ee7584cf6
Ana Silveira <ana.ccord@gmail.com>
2022-06-10 17:40:38 -0300

    Added Maratona Explorer full repo

---
+++ README.md
@@ -0,0 +1,5 @@
+## Um projeto desenvolvido na Maratona Explorer da Rocketseat
+
+Aqui está o resultado igual ao que o Mayk codou, pronto para você clonar e se divertir customizando! 💜 🪐
+
+Bora codar?
---
+++ script.js
@@ -0,0 +1,53 @@
+//variáveis
+const elementoResposta = document.querySelector("#resposta")
+
+const inputPergunta = document.querySelector("#inputPergunta")
+
+const respostas = [
+    "Certeza!",
+    "Não tenho tanta certeza.",
+    "É decididamente assim.",
+    "Não conte com isso.",
+    "Sem dúvidas!",
+    "Pergunte novamente mais tarde.",
+    "Sim, definitivamente!",
+    "Minha resposta é não.",
+    "Você pode contar com isso.",
+    "Melhor não te dizer agora.",
+    "A meu ver, sim.",
+    "Minhas fontes dizem não.",
+    "Provavelmente.",
+    "Não é possível prever agora.",
+    "Perspectiva boa.",
+    "As perspectivas não são tão boas.",
+    "Sim.",
+    "Concentre-se e pergunte novamente.",
+    "Sinais apontam que sim.",
+  ]
+
+
+//gerar número aleatório e fazer pergunta ao clicar
+function fazerPergunta() {
+
+
+    if(inputPergunta.value == ""){
+        alert("Digite sua pergunta")
+        return
+    }
+
+    const pergunta = "<div>" + inputPergunta.value + "</div>"
+
+
+    const totalRespostas = respostas.length
+    const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+    
+    elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
+
+    // sumir a reposta depois de um tempo
+
+    setTimeout(function() {
+        elementoResposta.style.opacity = 0
+    }, 3000)
+}
+
+
